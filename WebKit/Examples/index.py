cc071aeeb56ff6c7a0ef487ba3d06ef92e64d2b9
Christoph Zwerschke <cito@online.de>
2019-08-25 15:49:05 +0200

    Improve redirection in Example index servlet

--- WebKit/Examples/index.py
+++ WebKit/Examples/index.py
@@ -4,8 +4,13 @@ from WebKit.HTTPServlet import HTTPServlet
 class index(HTTPServlet):
 
     def respond(self, trans):
-        newPath = '/Examples/Welcome' + trans.request().extraURLPath()
-        # redirection via the server:
-        trans.application().forward(trans, newPath)
-        # redirection via the client:
-        # trans.response().sendRedirect(newPath)
+        extraPath = trans.request().extraURLPath()
+        path = trans.request().urlPath()
+        if path.endswith(extraPath):
+            path = path[:-len(extraPath)]
+        if not path.endswith('Welcome'):
+            path = path.rpartition('/')[0] + '/Welcome' + extraPath
+            # redirection via the server:
+            trans.application().forward(trans, path)
+            # redirection via the client:
+            # trans.response().sendRedirect(path)
