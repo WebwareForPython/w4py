02bade45bc459d89c76c96fc00a3143fd7da3667
Christoph Zwerschke <cito@online.de>
2019-09-01 13:25:25 +0200

    Yet another index servlet redirection fix

--- WebKit/Examples/index.py
+++ WebKit/Examples/index.py
@@ -6,7 +6,7 @@ class index(HTTPServlet):
     def respond(self, trans):
         extraPath = trans.request().extraURLPath()
         path = trans.request().urlPath()
-        if path.endswith(extraPath):
+        if extraPath and path.endswith(extraPath):
             path = path[:-len(extraPath)]
         if not path.endswith('Welcome'):
             path = path.rpartition('/')[0] + '/Welcome' + extraPath
